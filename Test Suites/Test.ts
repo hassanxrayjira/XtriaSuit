8fc6b6b730d358362b7b1787693c2bd317c6b3cb
jalal-hussain <jalalhussain781@gmail.com>
2023-07-10 01:17:02 +0500

    Commit

--- Test Suites/Test.ts
+++ Test Suites/Test.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>TEst</name>
+   <name>Test</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
